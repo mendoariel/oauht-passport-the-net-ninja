69cd23f9c172291da2c761e68c039bb87ae0f8b7
mendoariel <aarce@alquilando.com>
2021-02-01 01:57:26 -0300

    lesson 4 complete

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -6,7 +6,7 @@ router.get('/login', (req, res ) => {
 })
 
 // auth logout
-router.get('/logout', (res,req) => {
+router.get('/logout', (req,res) => {
     // handle with passport
     res.send('logging out')
 })
